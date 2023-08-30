e690bdb1ac2c8abe082419940ff1d4d3b5f3402a
Arkadiy Alaverdyan <AAlaverdyan@ibs.ru>
2023-08-30 17:58:57 +0300

    first commit

---
+++ src/test/java/ru/ibs/qa/runner/TestRunner.java
@@ -0,0 +1,16 @@
+package ru.ibs.qa.runner;
+
+import org.junit.platform.suite.api.*;
+
+import static io.cucumber.core.options.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@ConfigurationParameters({
+        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@fail"),
+        @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features"),
+        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "ru.ibs.qa.steps"),
+        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
+})
+public class TestRunner {
+}
